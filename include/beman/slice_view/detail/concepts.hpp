a9e2afac1b04ea15908c2b1e387f3c6c6662b0cf
ashu3103 <ash2003sharma@gmail.com>
2026-02-13 17:48:30 +0530

    define new is_empty_view concept

---
+++ include/beman/slice_view/detail/concepts.hpp
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#ifndef BEMAN_SLICE_VIEW_DETAIL_CONCEPT_HPP
+#define BEMAN_SLICE_VIEW_DETAIL_CONCEPT_HPP
+
+#include <concepts>
+#include <iterator>
+#include <ranges>
+#include <type_traits>
+
+namespace beman::slice_view::views {
+    namespace detail {
+
+        // trait templates
+        template<template<typename...> class Template, typename T>
+        struct is_specialization_of : std::false_type {};
+
+        template<template<typename...> class Template, typename... Args>
+        struct is_specialization_of<Template, Template<Args...>>
+            : std::true_type {};
+
+        // is empty
+        template <typename T>
+        concept is_empty_view =
+        is_specialization_of<
+            std::ranges::empty_view,
+            std::__remove_cvref_t<T>
+        >::value;
+
+        // 
+
+    } // namespace detail
+}
+
+#endif // BEMAN_SLICE_VIEW_DETAIL_CONCEPT_HPP
