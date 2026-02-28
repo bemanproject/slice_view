851d98010960240d3a4e46f1281358f97afe6f90
ashu3103 <ash2003sharma@gmail.com>
2026-02-28 09:05:57 +0530

    added slice_view_direct_usage and removed identity counterpart

---
+++ examples/slice_view_direct_usage.cpp
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include <beman/slice_view/slice_view.hpp>
+#include <iostream>
+#include <string_view>
+
+namespace exe = beman::slice_view;
+
+int main()
+{
+    std::string_view text = "Hello, world!";
+    auto s = text | exe::views::slice(7, 12);  // world
+
+    for (char c : s)
+        std::cout << c;
+
+    std::cout << '\n';
+
+    return 0;
+}
